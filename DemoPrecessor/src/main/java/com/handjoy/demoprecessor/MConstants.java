7689b43571ce96fd5f3a75b2451ca193e2c4cd40
596672125 <596672125@qq.com>
2019-01-19 12:27:16 +0800

    reflect:reflect view bingding for more than one acvity

---
+++ DemoPrecessor/src/main/java/com/handjoy/demoprecessor/MConstants.java
@@ -0,0 +1,16 @@
+package com.handjoy.demoprecessor;
+
+import com.squareup.javapoet.ClassName;
+
+public class MConstants {
+
+
+    public static ClassName CLASSNAME_LOG       = ClassName.get("android.util", "Log");
+
+    public static ClassName CLASSNAME_UI_THREAD = ClassName.get("android.support.annotation", "UiThread");
+
+    public static ClassName CLASSNAME_VIEW      = ClassName.get("android.view", "View");
+
+    public static String INJECT_NAME            ="inject";
+
+}
---
+++ app/src/main/java/com/handjoy/annotationdemo/two/TwoActivity.java
@@ -0,0 +1,35 @@
+package com.handjoy.annotationdemo.two;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.handjoy.annotationdemo.R;
+import com.handjoy.demoannotation.BindView;
+import com.handjoy.demoannotation.OnClick;
+
+public class TwoActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.hello)
+    TextView mTv;
+    @BindView(R.id.test)
+    Button mBtnTest;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TwoActivity_ViewBinding.inject(this);
+    }
+
+    @OnClick(R.id.hello)
+    void onViewClick(View v){
+        Log.i("test","hello");
+    }
+
+}
